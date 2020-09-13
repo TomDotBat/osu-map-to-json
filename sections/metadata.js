cf39cdd9cab033d7a47c8dbdd5fce508d7fa0a58
Tom <tom@tomdotbat.dev>
2020-09-13 22:26:19 +0100

    Metadata parsing

--- sections/metadata.js
+++ sections/metadata.js
@@ -1,2 +1,19 @@
 
 exports.name = "Metadata";
+
+exports.data = {};
+
+exports.reader = (line) => {
+    let splitData = line.split(":");
+
+    if (splitData[0] == "Tags") {
+        this.data[splitData[0]] = splitData[1].substring(0, splitData[1].length - 1).split(" ");
+        return;
+    }
+    else if (splitData[0] == "BeatmapID" || splitData[0] == "BeatmapSetID") {
+        this.data[splitData[0]] = parseInt(splitData[1]);
+        return;
+    }
+
+    this.data[splitData[0]] = splitData[1].substring(0, splitData[1].length - 1);
+}
