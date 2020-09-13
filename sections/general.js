1b0e9ec8483fa33a28be0a6f5e60196d8ba75f41
Tom <tom@tomdotbat.dev>
2020-09-13 22:16:56 +0100

    General section parsing

--- sections/general.js
+++ sections/general.js
@@ -1,2 +1,9 @@
 
 exports.name = "General";
+
+exports.data = {};
+
+exports.reader = (line) => {
+    let splitData = line.split(": ");
+    this.data[splitData[0]] = isNaN(splitData[1]) ? splitData[1].substring(0, splitData[1].length - 1) : parseInt(splitData[1]);
+}
