172be8df4a89464afce83118cd1f38e26fe8e340
Tom <tom@tomdotbat.dev>
2020-09-13 22:52:07 +0100

    Colour section parsing

--- sections/colours.js
+++ sections/colours.js
@@ -1,2 +1,21 @@
 
 exports.name = "Colours";
+
+exports.data = {};
+
+exports.reader = (line) => {
+    let splitData = line.split(" : ");
+    let colourValues = splitData[1].split(",")
+
+    for (let i = 0; i < colourValues.length; i++) {
+        colourValues[i] = parseInt(colourValues[i]);
+    }
+
+    if (splitData[0].startsWith("Combo")) {
+        if (!this.data["Combos"]) this.data["Combos"] = [];
+        this.data["Combos"][this.data["Combos"].length] = colourValues
+        return;
+    }
+
+    this.data[splitData[0]] = colourValues
+}
